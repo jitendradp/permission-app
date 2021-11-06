469ed997b09d41ca5cfd03df576373578e9f52d0
jitendradp <jitendradprajapati@yahoo.com>
2021-11-06 11:08:32 +0530

    3 files modified

--- src/app/node.model.ts
+++ src/app/node.model.ts
@@ -1,4 +1,7 @@
+import { FormGroup } from '@angular/forms';
+
 export class Node {
   children: Node[];
   text: string;
+  form?: FormGroup;
 }
--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Node } from './../node.model';
 @Component({
   selector: 'tree',
@@ -6,76 +7,126 @@ import { Node } from './../node.model';
   styleUrls: ['./tree.component.css'],
 })
 export class TreeComponent implements OnInit {
-  tree: Node[] = [
-    {
-      children: [
-        {
-          children: [
-            {
-              children: [],
-              text: 'firstNode',
-            },
-            {
-              children: [],
-              text: 'firstNode',
-            },
-          ],
-          text: 'firstNode',
-        },
-        {
-          children: [],
-          text: 'firstNode',
-        },
+  personalForm: FormGroup;
+  tree: Node[] = [];
+
+  constructor(public formBuilder: FormBuilder) {
+    this.personalForm = this.formBuilder.group({
+      firstName: [
+        '',
+        [Validators.required, Validators.minLength(2), Validators.maxLength(8)],
       ],
-      text: 'firstNode',
-    },
-    {
-      children: [],
-      text: 'Second Node',
-    },
-    {
-      children: [],
-      text: 'Third Node',
-    },
-    {
-      children: [],
-      text: 'Fourth Node',
-    },
-    {
-      children: [
-        {
-          children: [],
-          text: 'firstNode',
-        },
-        {
-          children: [],
-          text: 'firstNode',
-        },
-        {
-          children: [],
-          text: 'firstNode',
-        },
-        {
-          children: [],
-          text: 'firstNode',
-        },
+      lastName: [
+        '',
+        [Validators.required, Validators.minLength(2), Validators.maxLength(8)],
       ],
-      text: 'Fifth child',
-    },
-  ];
+      address: ['', [Validators.required]],
+      other: this.formBuilder.array([this.addOtherSkillFormGroup()]),
+    });
+    (<FormArray>this.personalForm.get('other')).push(
+      this.addOtherSkillFormGroup()
+    );
+    (<FormArray>this.personalForm.get('other')).push(
+      this.addOtherSkillFormGroup()
+    );
+    (<FormArray>this.personalForm.get('other')).push(
+      this.addOtherSkillFormGroup()
+    );
+  }
   ngOnInit() {
+    this.getControl();
+
     let firstNode: Node = {
       children: [],
       text: 'Sixth Node',
+      
     };
+    this.setForm();
     this.tree.push(firstNode);
   }
 
+  addOtherSkillFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      name: ['test', Validators.required],
+      edit: [true, Validators.required],
+      view: [true, Validators.required],
+    });
+  }
   addNodeTo() {
-    let newNode: Node = {
-      children: [],
-      text: 'newNode',
-    };
-    this.tree.push(newNode);
+    // this.addOtherSkillFormGroup();
+    (<FormArray>this.personalForm.get('other')).push(
+      this.addOtherSkillFormGroup()
+    );
+    // let newNode: Node = {
+    //   children: [],
+    //   text: 'newNode',
+    // };
+    // this.tree.push(newNode);
+  }
+
+  public getControl(index: number = 0) {
+    let otherForm = this.personalForm.get('other') as FormArray;
+    let formGroup = otherForm.at(index);
+    console.log(formGroup);
+  }
+
+  setForm() {
+    this.tree = [
+      // {
+      //   children: [
+      //     {
+      //       children: [
+      //         {
+      //           children: [],
+      //           text: 'firstNode',
+      //         },
+      //         {
+      //           children: [],
+      //           text: 'firstNode',
+      //         },
+      //       ],
+      //       text: 'firstNode',
+      //     },
+      //     {
+      //       children: [],
+      //       text: 'firstNode',
+      //     },
+      //   ],
+      //   text: 'firstNode',
+      // },
+      {
+        children: [],
+        text: 'Second Node',
+      },
+      {
+        children: [],
+        text: 'Third Node',
+      },
+      {
+        children: [],
+        text: 'Fourth Node',
+      },
+      // {
+      //   children: [
+      //     {
+      //       children: [],
+      //       text: 'firstNode',
+      //     },
+      //     {
+      //       children: [],
+      //       text: 'firstNode',
+      //     },
+      //     {
+      //       children: [],
+      //       text: 'firstNode',
+      //     },
+      //     {
+      //       children: [],
+      //       text: 'firstNode',
+      //     },
+      //   ],
+      //   text: 'Fifth child',
+      // },
+    ];
   }
 }
